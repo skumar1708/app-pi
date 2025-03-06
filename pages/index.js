5e0e98e632f1a9f14a8628ee51e59fcdf1d97b54
skumar1708 <itbhu.sk@gmail.com>
2025-03-06 12:56:26 +0530

    STatic url changed

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
   const textareaRef = useRef(null);
   const [appName, setAppName] = useState("");
 
-  const API_BASE_URL = "http://localhost:5000";//"https://app-generator-backend.vercel.app";
+  const API_BASE_URL = "https://app-generator-backend.vercel.app";
 
   const handleGenerate = async () => {
     setStatusMessages([]);
